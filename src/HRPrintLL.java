e8d4f61000d55a519fc942afff2af7daa8c4d08b
Cameron Powell <cameronspowell@gmail.com>
2017-09-13 00:51:17 -0400

    solved

---
+++ src/HRPrintLL.java
@@ -0,0 +1,14 @@
+class HRPrintLL {
+    class Node {
+        int data;
+        Node next;
+    }
+
+    void Print(Node head) {
+        Node current = head;
+        while(current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
