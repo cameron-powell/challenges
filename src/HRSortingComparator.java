33c37da0206cb6c8e693c105f8fec8fb5852cea2
Cameron Powell <cameronspowell@gmail.com>
2017-10-18 21:08:22 -0400

    solved

---
+++ src/HRSortingComparator.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player p1, Player p2) {
+        return -1 * Integer.compare(p1.score, p2.score);
+    }
+}
+
+class Player{
+    String name;
+    int score;
+
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class HRSortingComparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
